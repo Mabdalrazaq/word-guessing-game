744738b701703d5645306f052fdfabaf4f3214d5
Mabd <m.abdalrazaq314@gmail.com>
2022-02-05 12:43:01 +0100

    manually cherry-picked some useful features from original repo

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -9,17 +9,17 @@ type Props = {
 
 export const Cell = ({ value, status, isHidden }: Props) => {
   const classes = classnames(
-    'h-5 w-5 lg:w-14 lg:h-14 md:w-12 md:h-12 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded dark:text-white',
+    'h-5 w-5 lg:w-14 lg:h-14 md:w-12 md:h-12 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
       'bg-green-500 text-white border-green-500': status === 'correct-all',
       'bg-red-500 text-white border-red-500': status === 'incorrect-all',
       'bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600':
         !status,
       'border-black dark:border-slate-100': value && !status,
-      'bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
+      'shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
         status === 'absent',
-      'bg-blue-500 text-white border-blue-500': status === 'correct',
-      'bg-orange-500 dark:bg-orange-700 text-white border-orange-500 dark:border-orange-700':
+      'shadowed bg-blue-500 text-white border-blue-500': status === 'correct',
+      'shadowed bg-orange-500 dark:bg-orange-700 text-white border-orange-500 dark:border-orange-700':
         status === 'present',
       'cell-animation': !!value,
     }
