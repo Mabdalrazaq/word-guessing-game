50cacf1809e4229fd10b81ec771847e90ac1a936
Mabd <m.abdalrazaq314@gmail.com>
2022-02-05 23:36:31 +0100

    edit typo in info

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,7 +10,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The Computer guessed the word after a couble of tries.
+        The Computer guessed the word after a couple of tries.
         After each guess, the color of the tiles changed to show how close its guess was to the word.
       </p>
 
