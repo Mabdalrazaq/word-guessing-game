ca7d6be4720235d4c4636ee6b28885246ada12d1
Mabd <m.abdalrazaq314@gmail.com>
2022-02-05 21:23:47 +0100

    updated about and info

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,13 +9,24 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game -{' '}
+        eldroW is a game with opposite mechanics of the famous game&nbsp;
+        <a href='https://www.powerlanguage.co.uk/wordle/'
+          className="underline font-bold">Wordle
+        </a>. Hence, the name.
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Developed by&nbsp;
+        <a
+          href="https://www.linkedin.com/in/mohammad-abdallah-81743b148/"
+          className="underline font-bold">
+          Mohammad Abdallah
+        </a>
+        &nbsp;with the help of&nbsp;
         <a
           href="https://github.com/hannahcode/GAME"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
+          className="underline font-bold">
+          this Wordle Clone
+        </a>
       </p>
     </BaseModal>
   )
--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        The Computer guessed the word after a couble of tries.
+        After each guess, the color of the tiles changed to show how close its guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -22,7 +22,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter W was in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -33,7 +33,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter L was in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -43,9 +43,17 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="U" status="absent" />
         <Cell value="E" />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+      <p className="mb-1 text-sm text-gray-500 dark:text-gray-300">
+        The letter U was not in the word in any spot.
       </p>
+
+      <p className="font-bold mt-1 mb-1 text-sm text-gray-500 dark:text-gray-300 m">
+        Your goal now is to guess the guesses, using only the colors the computer revealed, and the solution!
+      </p>
+      <p className="italic mt-1 text-xs text-gray-500 dark:text-gray-300 m">
+        Hint: The computer is not stupid; It would never guess a word with information contradicting with the feedback it got from guessing the previous word
+      </p>
+
     </BaseModal>
   )
 }
